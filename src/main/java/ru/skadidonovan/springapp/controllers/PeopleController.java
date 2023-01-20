12d2056a31ad3c9ce7de53aadd232bc2450486ff
SkadiDonovan <fant1maz@yandex.ru>
2023-01-20 14:49:39 +0300

    added add, delete and update methods

--- src/main/java/ru/skadidonovan/springapp/controllers/PeopleController.java
+++ src/main/java/ru/skadidonovan/springapp/controllers/PeopleController.java
@@ -2,10 +2,9 @@ package ru.skadidonovan.springapp.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import ru.skadidonovan.springapp.dao.PersonDAO;
+import ru.skadidonovan.springapp.models.Person;
 
 @Controller
 @RequestMapping("/people")
@@ -28,4 +27,33 @@ public class PeopleController {
         model.addAttribute("person", personDAO.show(id));
         return "people/show";
     }
+
+    @GetMapping("/new")
+    public String newPerson(@ModelAttribute Person person) {
+        return "people/new";
+    }
+
+    @PostMapping
+    public String create(@ModelAttribute Person person) {
+        personDAO.save(person);
+        return "redirect:/people";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String edit(Model model, @PathVariable("id") int id){
+        model.addAttribute("person", personDAO.show(id));
+        return "people/edit";
+    }
+    @PatchMapping("/{id}")
+    public String update(@ModelAttribute("person") Person person, @PathVariable("id") int id){
+        personDAO.update(id, person);
+        return "redirect:/people";
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable("id") int id){
+        personDAO.delete(id);
+        return "redirect:/people";
+    }
+
 }
